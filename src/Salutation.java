6211c1a89b5efa8395aa03b3712823195ea060df
Tommy-BOU <tommy.boubaker@free.fr>
2025-06-20 17:11:50 +0200

    version de tommy

--- src/Salutation.java
+++ src/Salutation.java
@@ -1,5 +1,12 @@
 public class Salutation {
     public static void main(String[] args) {
         System.out.println("Hello, user!");
+
+        public void Hello(String name) {
+            System.out.println("Bonjour de " + name + " !");
+        }
+        
+        Hello("Tommy");
+
     }
 }
